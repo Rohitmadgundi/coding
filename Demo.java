961233eaa533f56c43323ea0d16bc033b0fe7fd2
Rohit <45898595+Rohitmadgundi@users.noreply.github.com>
2026-03-29 12:20:38 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -23,7 +23,7 @@ public class Demo{
     public static void main(String[] args) {
 
         String s1 = "rohit";
-        String s2 = "madam";
+        String s2 = "mada1";
 
         System.out.println(s1 + " = " + isPalindrome(s1));
         System.out.println(s2 + " = " + isPalindrome(s2));
